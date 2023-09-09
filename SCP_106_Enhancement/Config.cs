f5cdb5c12312163f838b21db21f1ef40d36512d2
aalonee <alone@evolution-project.online>
2023-09-09 16:46:36 +0300

    Добавьте файлы проекта.

---
+++ SCP_106_Enhancement/Config.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace scp106
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        [Description("The hint text to be sent to the player who was attacked by SCP-106")]
+        public string CaughtHintText { get; set; } = "You have been caught in a pocket dimension by <color=red>%attacker%</color>";
+
+        [Description("Caught to PD player hint duration")]
+        public float CaughtHintDuration { get; set; } = 5.0F;
+
+        [Description("A hint to send to the player who escaped from the PD")]
+        public string EscapedHintText { get; set; } = "<color=green>Congratulations! You have escaped from the pocket dimension!</color>";
+
+        [Description("Escaped PD hint duration")]
+        public float EscapedHintDuration { get; set; } = 5.0F;
+    }
+}
---
+++ SCP_106_Enhancement/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.Commands.Reload;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp049;
+using PlayerRoles;
+using PlayerRoles.PlayableScps.Scp106;
+using PluginAPI.Events;
+using static MapGeneration.ImageGenerator;
+
+namespace scp106
+{
+    public sealed class Plugin : Plugin<Config>
+    {
+        public override PluginPriority Priority { get; } = PluginPriority.Lower;
+        public override string Name { get; } = "SCP 106 Enhancement";
+
+        public override string Author { get; } = "alone";
+
+        public override string Prefix { get; } = "SCP106_Enhancement";
+
+        public override void OnEnabled()
+        {
+            Log.Info("Plugin enabled");
+            Exiled.Events.Handlers.Player.Hurting += OnAttack;
+            Exiled.Events.Handlers.Player.EscapingPocketDimension += OnEscaping;
+        }
+
+        public override void OnDisabled()
+        {
+            Log.Info("Plugin disabled");
+        }
+
+        public void OnEscaping(EscapingPocketDimensionEventArgs ev)
+        {
+            ev.Player.ShowHint(Config.EscapedHintText, Config.EscapedHintDuration);
+        }
+
+        public void OnAttack(HurtingEventArgs ev)
+        {
+            if (ev.Attacker.Role == RoleTypeId.Scp106)
+            {
+                ev.Player.ShowHint(Config.CaughtHintText.Replace("%attacker%", ev.Attacker.Nickname), Config.CaughtHintDuration);
+                ev.Player.EnableEffect(EffectType.Corroding);
+            }
+        }
+    }
+}
